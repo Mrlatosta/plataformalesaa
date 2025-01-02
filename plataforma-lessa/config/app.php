a231fdded7b544bf2418f254f295531c8c71b3f3
Iván Meraz <pataponivan@gmail.com>
2025-01-02 13:13:23 -0500

    Deploy Cambios env

--- plataforma-lessa/config/app.php
+++ plataforma-lessa/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://king-prawn-app-no8on.ondigitalocean.app/'),
 
     /*
     |--------------------------------------------------------------------------
