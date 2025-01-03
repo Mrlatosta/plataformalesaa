f18c7237271e674e89405e3e0ad361e71e690a06
Iván Meraz <pataponivan@gmail.com>
2025-01-03 12:31:31 -0500

    'vite cambios

--- plataforma-lessa/vite.config.js
+++ plataforma-lessa/vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [
-        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
+        vue(),
     ],
-});
+    server: command === 'serve' ? {
+        host: '0.0.0.0',
+        port: 5173,
+    } : undefined,
+}));
